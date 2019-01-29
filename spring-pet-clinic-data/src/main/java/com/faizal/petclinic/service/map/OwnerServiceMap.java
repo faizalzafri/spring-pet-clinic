944550be9053a68437fc63a42e927e0405da5ac9
Faizal <zafri_faizal@outlook.com>
2019-01-29 23:38:55 +0530

    Closes #6

---
+++ spring-pet-clinic-data/src/main/java/com/faizal/petclinic/service/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package com.faizal.petclinic.service.map;
+
+import java.util.Set;
+
+import com.faizal.petclinic.model.Owner;
+import com.faizal.petclinic.service.CrudService;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+	@Override
+	public Set<Owner> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return super.findByID(id);
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public void delete(Owner object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+}
