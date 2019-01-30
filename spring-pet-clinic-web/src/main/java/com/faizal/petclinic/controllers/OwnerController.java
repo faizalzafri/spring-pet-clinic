c4bc485ebefe2168331ce172292a14114b9f3e9c
Faizal <zafri_faizal@outlook.com>
2019-01-30 23:39:16 +0530

    added display of owners. Closes #14

--- spring-pet-clinic-web/src/main/java/com/faizal/petclinic/controllers/OwnerController.java
+++ spring-pet-clinic-web/src/main/java/com/faizal/petclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.faizal.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.faizal.petclinic.service.OwnerService;
 
 @Controller
 public class OwnerController {
 
-	@RequestMapping({ "/owners", "/owners/", "/owners/index", "/owners/index.html" })
-	public String listOwners() {
+	private final OwnerService ownerService;
 
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+	@RequestMapping({ "/owners", "/owners/", "/owners/index", "/owners/index.html" })
+	public String listOwners(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 }
