afd1da31613514b92ea8492dfac8b63b93aa9287
Faizal <zafri_faizal@outlook.com>
2019-01-30 23:44:02 +0530

    added vets display. Close #15

--- spring-pet-clinic-web/src/main/java/com/faizal/petclinic/controllers/VetController.java
+++ spring-pet-clinic-web/src/main/java/com/faizal/petclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package com.faizal.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.faizal.petclinic.service.VetService;
 
 @Controller
 public class VetController {
 
-	@RequestMapping({ "/vets", "/vets/", "/vets/index", "/vets/index.html" })
-	public String listVets() {
+	private final VetService vetService;
 
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({ "/vets", "/vets/", "/vets/index", "/vets/index.html" })
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
