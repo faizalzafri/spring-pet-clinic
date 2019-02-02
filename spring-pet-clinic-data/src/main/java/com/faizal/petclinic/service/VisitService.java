8f2906c523ccfe8a0e866aad8ad55b6f7b9c0aa9
Faizal <zafri_faizal@outlook.com>
2019-02-02 11:32:29 +0530

    adding Visit Map Service. Closes #45

---
+++ spring-pet-clinic-data/src/main/java/com/faizal/petclinic/service/VisitService.java
@@ -0,0 +1,7 @@
+package com.faizal.petclinic.service;
+
+import com.faizal.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ spring-pet-clinic-data/src/main/java/com/faizal/petclinic/service/map/VisitServiceMap.java
@@ -0,0 +1,54 @@
+package com.faizal.petclinic.service.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.faizal.petclinic.model.Pet;
+import com.faizal.petclinic.model.Visit;
+import com.faizal.petclinic.service.PetService;
+import com.faizal.petclinic.service.PetTypeService;
+import com.faizal.petclinic.service.VisitService;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+	private final PetTypeService petTypeService;
+	private final PetService petService;
+
+	public VisitServiceMap(PetTypeService petTypeService, PetService petService) {
+		super();
+		this.petTypeService = petTypeService;
+		this.petService = petService;
+	}
+
+	@Override
+	public Set<Visit> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		return super.findByID(id);
+	}
+
+	@Override
+	public Visit save(Visit object) {
+
+		if (object.getPet() != null || object.getPet().getOwner() != null || object.getPet().getId() != null) {
+			throw new RuntimeException("Invalid Visit");
+		}
+		return super.save(object);
+	}
+
+	@Override
+	public void delete(Visit object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+}
